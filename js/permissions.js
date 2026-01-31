9328378a39c86a5babb91c24ec67a47fe7da23df
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 16:53:54 -0500

    Create permissions.js

---
+++ js/permissions.js
@@ -0,0 +1,8 @@
+// /js/permissions.js
+export const PERMISSIONS = {
+  dashboard: ["admin_root", "admin"],
+  cierre_turno: ["admin_root", "admin", "operativo"],
+  configuracion: ["admin_root"],
+  inventarios: ["admin_root", "admin", "operativo"],
+  nomina: ["admin_root", "admin"]
+};
