5966d692b9e190435a77a643c7cf984aa61b4276
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 14:08:28 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,15 @@
+import { supabase } from "./supabase.js";
 
+async function loadHeader() {
+  const res = await fetch("../header.html");
+  const html = await res.text();
+
+  document.body.insertAdjacentHTML("afterbegin", html);
+
+  document.getElementById("logoutBtn").addEventListener("click", async () => {
+    await supabase.auth.signOut();
+    window.location.href = "../";
+  });
+}
+
+loadHeader();
