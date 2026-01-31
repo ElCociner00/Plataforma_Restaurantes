606cc0c153392c369952e5d2b1625578801edbab
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 12:24:51 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,20 @@
+import { supabase } from "./supabase.js";
 
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert("Credenciales incorrectas");
+    return;
+  }
+
+  window.location.href = "/dashboard/";
+});
