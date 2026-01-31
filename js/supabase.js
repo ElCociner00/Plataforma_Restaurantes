d987b58a86162f64a1d69a1b2e3531944a79a8f0
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 12:31:40 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://ivgzwgyjyqfunheaesxx.supabase.co";
 
 export const supabase = createClient(
-  "https://TU_PROYECTO.supabase.co",
-  "TU_ANON_PUBLIC_KEY"
+  "https://ivgzwgyjyqfunheaesxx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2Z3p3Z3lqeXFmdW5oZWFlc3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NjAxMDUsImV4cCI6MjA4NTQzNjEwNX0.5Q-MQ7fKfCG9Qo09G_vub3-Rn6FHLJ18sf8eKGndhbI"
 );
