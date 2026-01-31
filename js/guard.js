3d7a0ffb0c72ceaca731f375257cc6c14e8a17d0
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 16:53:12 -0500

    Delete js/guard.js

--- js/guard.js
+++
@@ -1,8 +0,0 @@
-import { getUserContext } from "./session.js";
-
-const context = await getUserContext();
-console.log("GUARD DASHBOARD:", context);
-
-if (!context) {
-  window.location.href = "/Plataforma_Restaurantes/login/";
-}
