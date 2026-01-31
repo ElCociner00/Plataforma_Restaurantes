3396e24c6e49cbc09dc61a233f45a4542e6d1ca2
ElCociner00 <santiagoelchameluco@gmail.com>
2026-01-31 15:15:02 -0500

    Update guard_operativo.js

--- js/guard_operativo.js
+++ js/guard_operativo.js
@@ -5,3 +5,8 @@ const context = await getUserContext();
 if (!context) {
   window.location.href = "/Plataforma_Restaurantes/login/";
 }
+
+if (context.rol !== "operativo" && context.rol !== "admin") {
+  alert("No tienes permisos para este módulo");
+  window.location.href = "/Plataforma_Restaurantes/dashboard/";
+}
